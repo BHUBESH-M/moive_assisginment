9d630c1f42f7ce8ce3b8640e405acff515817e31
BHUBESH-M <140890117+BHUBESH-M@users.noreply.github.com>
2025-07-18 12:26:38 +0530

    Update App.js

--- placement/src/compoents/App.js
+++ placement/src/compoents/App.js
@@ -1,1 +1,12 @@
 
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h2>This is an unused App component inside components folder.</h2>
+    </div>
+  );
+};
+
+export default App;
